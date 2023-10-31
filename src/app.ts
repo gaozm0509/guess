57693d9239e3f88737dca4970b80563b0e732ac2
高泽民 <gaozemin0509@gmail.com>
2023-10-31 21:18:51 +0800

    首页 UI

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
+/*
+ * @Author: 农村高富帅
+ * @Date: 2023-10-31 17:32:46
+ * @LastEditors: 农村高富帅
+ * @LastEditTime: 2023-10-31 20:07:10
+ * @FilePath: /guess/src/app.ts
+ * @Description: 
+ * @mail: gaozemin0509@gmail.com
+ */
 import { createApp } from 'vue'
 import './app.scss'
+import { createUI } from 'taro-ui-vue3'
+
+// 引用全部组件样式
+import 'taro-ui-vue3/dist/style/index.scss'
 
 const App = createApp({
   onShow (options) {},
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
+// 引用全部组件
+const tuv3 = createUI()
+App.use(tuv3)
 
 export default App
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,19 +1,32 @@
+<!--
+ * @Author: 农村高富帅
+ * @Date: 2023-10-31 17:32:46
+ * @LastEditors: 农村高富帅
+ * @LastEditTime: 2023-10-31 21:17:39
+ * @FilePath: /guess/src/pages/index/index.vue
+ * @Description: 首页
+ * @mail: gaozemin0509@gmail.com
+-->
 <template>
-  <view class="index">
-    <text>{{ msg }}</text>
+  <view class="index page">
+    <view class="create-view option">
+      <text> 创建房间</text>
+      <image :src=addImg svg="true"></image>
+    </view>
+    <view class="add-view option">
+      <text>加入房间</text>
+      <image :src=createimg svg="true"></image>
+    </view>
   </view>
 </template>
 
-<script>
-import { ref } from 'vue'
-import './index.scss'
-
-export default {
-  setup () {
-    const msg = ref('Hello world')
-    return {
-      msg
-    }
-  }
-}
+<script setup>
+import { ref } from "vue";
+import "./index.scss";
+import { View } from "@tarojs/components";
+// import addImg from "../../assets/images/add.svg";
+import createimg from "../../assets/images/create.svg";
+import addImg from "../../assets/images/add.svg";
 </script>
+
+
