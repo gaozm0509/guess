93c456a770f34a62f74ddb2ced3b35e93fccd6c6
高泽民 <gaozemin0509@gmail.com>
2023-11-01 22:08:39 +0800

    :lipstick: 修改首页 UI

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,20 +2,26 @@
  * @Author: 农村高富帅
  * @Date: 2023-10-31 17:32:46
  * @LastEditors: 农村高富帅
- * @LastEditTime: 2023-10-31 21:17:39
+ * @LastEditTime: 2023-11-01 21:20:31
  * @FilePath: /guess/src/pages/index/index.vue
  * @Description: 首页
  * @mail: gaozemin0509@gmail.com
 -->
 <template>
   <view class="index page">
-    <view class="create-view option">
-      <text> 创建房间</text>
-      <image :src=addImg svg="true"></image>
+    <view class="play-view option">
+      <text>开始游戏</text>
+      <image :src="playImg" svg="true"></image>
     </view>
-    <view class="add-view option">
-      <text>加入房间</text>
-      <image :src=createimg svg="true"></image>
+    <view class="bottom-view">
+      <view class="create-view option">
+        <text> 创建房间</text>
+        <image :src="addImg" svg="true"></image>
+      </view>
+      <view class="add-view option">
+        <text>加入房间</text>
+        <image :src="createimg" svg="true"></image>
+      </view>
     </view>
   </view>
 </template>
@@ -27,6 +33,7 @@ import { View } from "@tarojs/components";
 // import addImg from "../../assets/images/add.svg";
 import createimg from "../../assets/images/create.svg";
 import addImg from "../../assets/images/add.svg";
+import playImg from "../../assets/images/play.svg";
 </script>
 
 
